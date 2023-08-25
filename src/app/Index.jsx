6ee212554766b497fe41a16731178591cf6f3cdc
Per Steen Olsen <persteen22@hotmail.com>
2023-08-25 13:21:06 +0200

    Test nested router - a

--- src/app/Index.jsx
+++ src/app/Index.jsx
@@ -44,7 +44,10 @@ function App() {
                 
                 <Route path="/account" component={Account} />
 
-                <Route exact path="/gh-pages-react-node-orm-test" component={AboutPage} />
+                <Route exact path="/gh-pages-react-node-orm-test" component={AboutPage} >
+                     <Route exact path="/persteenolsen" component={MyInfoPage} />
+                </Route>  
+
                 <Route exact path="/about" component={AboutPage} />
                 
 				<Route exact path="/persteenolsen" component={MyInfoPage} />
